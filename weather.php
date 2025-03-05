1aeb117e48a5c57021926a2eddf802ed7eeb257a
Lucas Bastos <lucas@lucasbastos.com>
2025-03-04 20:59:34 -0500

    Check params

--- weather.php
+++ weather.php
@@ -1,21 +1,27 @@
 <?php
-$apikey = getenv('OPENWEATHER_API_KEY');
-$zip = $_GET["zip"];
-$country = $_GET["country"];
-$lat = $_GET["lat"];
-$lon = $_GET["lon"];
-$ge = $_GET["ge"];
-
-if ($ge=="false") {
-    $call = "https://api.openweathermap.org/data/2.5/weather?zip=".$zip.",".$country."&appid=".$apikey;
+if (!isset($_GET["zip"]) || !isset($_GET["country"]) || !isset($_GET["lat"]) || !isset($_GET["lon"]) || !isset($_GET["ge"])) {
+    echo "Error: Missing parameters";
+    exit();
 } else {
-    $call = "https://api.openweathermap.org/data/2.5/weather?lat=".$lat."&lon=".$lon."&appid=".$apikey;
-} 
+    $apikey = getenv('OPENWEATHER_API_KEY');
+    $zip = $_GET["zip"];
+    $country = $_GET["country"];
+    $lat = $_GET["lat"];
+    $lon = $_GET["lon"];
+    $ge = $_GET["ge"];
 
-$fp = fopen($call, "r");
-$contents = "";
-while ($more = fread($fp, 1000)) {
-    $contents .= $more;
+    if ($ge=="false") {
+        $call = "https://api.openweathermap.org/data/2.5/weather?zip=".$zip.",".$country."&appid=".$apikey;
+    } else {
+        $call = "https://api.openweathermap.org/data/2.5/weather?lat=".$lat."&lon=".$lon."&appid=".$apikey;
+    } 
+
+    $fp = fopen($call, "r");
+    $contents = "";
+    while ($more = fread($fp, 1000)) {
+        $contents .= $more;
+    }
+    echo $contents;
+    fclose($fp);
 }
-echo $contents;
 ?>
